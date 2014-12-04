07944c3ccf5ba64b7ef2864cf17154ab077807a8
unknown <User@UAVLab1.fzt.haw-hamburg.de>
2014-12-04 13:56:57 +0100

    Cleaned Eclipse Workspace

---
+++ prj/CleanEclipse.bat
@@ -0,0 +1,8 @@
+del .metadata\.plugins\org.eclipse.cdt.core\*.pdom
+del .metadata\.plugins\org.eclipse.cdt.core\*.log
+del .metadata\.plugins\org.eclipse.cdt.ui\*.log
+rmdir /s /q .metadata\.plugins\org.eclipse.core.resources\.history\
+del .metadata\.plugins\org.eclipse.epp.usagedata.recording\*.csv
+del .metadata\.bak_0.log
+del .metadata\.log
+PAUSE
